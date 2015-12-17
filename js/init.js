ce193d7d7a3fa19738d37e3312194121db0ad5bc
Thomas Huang <thomas.yt.huang@gmail.com>
2015-12-17 17:26:26 -0500

    start editing template

--- js/init.js
+++ js/init.js
@@ -8,37 +8,37 @@ jQuery(document).ready(function($) {
 
 /*---------------------------------------------------- */
 /* Preloader
------------------------------------------------------- */ 
+------------------------------------------------------ */
   	$(window).load(function() {
 
-   	// will first fade out the loading animation 
-    	$("#status").fadeOut("slow"); 
+   	// will first fade out the loading animation
+    	$("#status").fadeOut("slow");
 
-    	// will fade out the whole DIV that covers the website. 
-    	$("#preloader").delay(500).fadeOut("slow").remove();      
+    	// will fade out the whole DIV that covers the website.
+    	$("#preloader").delay(500).fadeOut("slow").remove();
 
-  	}) 
+  	})
 
 /*---------------------------------------------------- */
 /* Menu
------------------------------------------------------- */  
-  	var toggle_button = $("<a>", {                         
-                        id: "toggle-btn", 
+------------------------------------------------------ */
+  	var toggle_button = $("<a>", {
+                        id: "toggle-btn",
                         html : "Menu",
                         title: "Menu",
-                        href : "#" } 
+                        href : "#" }
                         );
   	var nav_wrap = $('nav#nav-wrap')
-  	var nav = $("ul#nav");  
+  	var nav = $("ul#nav");
 
-  	/* id JS is enabled, remove the two a.menu-btns 
+  	/* id JS is enabled, remove the two a.menu-btns
   	and dynamically prepend a.toggle-btn to #nav-wrap */
-  	nav_wrap.find('a.menu-btn').remove(); 
-  	nav_wrap.prepend(toggle_button); 
+  	nav_wrap.find('a.menu-btn').remove();
+  	nav_wrap.prepend(toggle_button);
 
   	toggle_button.on("click", function(e) {
    	e.preventDefault();
-    	nav.slideToggle("fast");     
+    	nav.slideToggle("fast");
   	});
 
   	if (toggle_button.is(':visible')) nav.addClass('mobile');
@@ -47,8 +47,8 @@ jQuery(document).ready(function($) {
     	else nav.removeClass('mobile');
   	});
 
-  	$('ul#nav li a').on("click", function(){      
-   	if (nav.hasClass('mobile')) nav.fadeOut('fast');      
+  	$('ul#nav li a').on("click", function(){
+   	if (nav.hasClass('mobile')) nav.fadeOut('fast');
   	});
 
 
@@ -56,7 +56,7 @@ jQuery(document).ready(function($) {
 /* Backstretch Settings
 ------------------------------------------------------ */
 
-	$("#intro").backstretch("images/header-background.jpg");
+	$("#intro").backstretch("images/beach.jpg");
 
 
 /*----------------------------------------------------*/
@@ -81,7 +81,7 @@ jQuery(document).ready(function($) {
 
 /*----------------------------------------------------*/
 /*  Placeholder Plugin Settings
------------------------------------------------------- */  	 
+------------------------------------------------------ */
 	$('input, textarea').placeholder()
 
 
@@ -91,23 +91,23 @@ jQuery(document).ready(function($) {
    setTimeout( function() {
 
 	   $('h1.responsive-headline').fitText(1, { minFontSize: '40px', maxFontSize: '90px' });
-	   
+
    }, 100);
 
 /*----------------------------------------------------*/
 /* Final Countdown Settings
 ------------------------------------------------------ */
-	var finalDate = '2016/01/01';
+	var finalDate = '2016/03/16';
 
 	$('div#counter').countdown(finalDate)
    	.on('update.countdown', function(event) {
 
-   		$(this).html(event.strftime('<span>%D <em>days</em></span>' + 
-   										 	 '<span>%H <em>hours</em></span>' + 
+   		$(this).html(event.strftime('<span>%D <em>days</em></span>' +
+   										 	 '<span>%H <em>hours</em></span>' +
    										 	 '<span>%M <em>minutes</em></span>' +
    										 	 '<span>%S <em>seconds</em></span>'));
 
-   });  
+   });
 
 
 /*----------------------------------------------------*/
@@ -118,13 +118,13 @@ jQuery(document).ready(function($) {
 
    // main directions
    map = new GMaps({
-      el: '#map', 
-      lat: 14.549072, 
-      lng: 121.046958, 
-      zoom: 14, 
+      el: '#map',
+      lat: 30.186170,
+      lng: -85.823288,
+      zoom: 9,
       zoomControl : true,
-      zoomControlOpt: { style : 'SMALL', position: 'TOP_LEFT' }, 
-      panControl : false, 
+      zoomControlOpt: { style : 'SMALL', position: 'TOP_LEFT' },
+      panControl : false,
       scrollwheel: false
    });
 
@@ -136,7 +136,7 @@ jQuery(document).ready(function($) {
 	   horizontalAlign: 'center'
 	});
 
-   // The styles below present a simplified map. 
+   // The styles below present a simplified map.
    // If you would like to use a normal coloured map, then please remove or comment the code below, from lines 128 to 148.
 	var mapStyles = [
 	   {
@@ -155,16 +155,16 @@ jQuery(document).ready(function($) {
 	      }]
 	   }
 	];
-	
+
 	map.setOptions({
 	   styles: mapStyles
 	});
 
-   // map.addMarker({ 
+   // map.addMarker({
  		// lat: map.getCenter().lat(),
-	  //  lng: map.getCenter().lng(), 
+	  //  lng: map.getCenter().lng(),
  		// title: '5th Avenue',
-   //  	infoWindow: { content: '<p>You can add your address 1 here</p>' } 
+   //  	infoWindow: { content: '<p>You can add your address 1 here</p>' }
    // });
 
 
@@ -224,7 +224,8 @@ jQuery(document).ready(function($) {
         $('#intro, #map').css({ 'height': $(window).height() });
         $('body').css({ 'width': $(window).width() })
 
-        $("#intro").backstretch("images/header-background.jpg");
+        // $("#intro").backstretch("images/header-background.jpg");
+        $("#intro").backstretch("images/beach.jpg");
    });
 
 
